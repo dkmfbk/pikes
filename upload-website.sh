856743af268a1a969231b12a3919fd11b5021505
aprosio <alessio@apnetwork.it>
2015-06-08 18:07:49 +0200

    Website script

---
+++ upload-website.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mvn clean package site:site site:deploy -DskipTests
+git checkout gh-pages
+cp -r target/website/* .
+git add *
+git commit -a -m "Website"
+git push
+git checkout master
