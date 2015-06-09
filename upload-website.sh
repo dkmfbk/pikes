d9282e056a4f6c7bca7ca75c3dfea617f3991e53
aprosio <alessio@apnetwork.it>
2015-06-09 12:30:07 +0200

    Script

--- upload-website.sh
+++ upload-website.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# set -e
+CHANGED=$(git diff-index --name-only HEAD --)
+if [ -n "$CHANGED" ]; then
+    echo "There are uncommitted changes on GIT. Commit them before releasing website."
+    exit
+fi
 
 mvn clean site:site site:deploy -DskipTests -Dsite.root=http://pikes.fbk.eu/
 git checkout gh-pages
