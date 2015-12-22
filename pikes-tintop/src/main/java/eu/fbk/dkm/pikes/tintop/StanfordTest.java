b250e322471b9c9806aafab6e8a055405215c95f
Alessio Palmero Aprosio <alessio@apnetwork.it>
2015-12-22 10:14:31 +0100

    Now it installs

--- pikes-tintop/src/main/java/eu/fbk/dkm/pikes/tintop/StanfordTest.java
+++ pikes-tintop/src/main/java/eu/fbk/dkm/pikes/tintop/StanfordTest.java
@@ -10,13 +10,11 @@ import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.semgraph.SemanticGraph;
 import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations;
 import edu.stanford.nlp.semgraph.SemanticGraphEdge;
-import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
 import edu.stanford.nlp.trees.CollinsHeadFinder;
 import edu.stanford.nlp.trees.HeadFinder;
 import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.util.ArrayCoreMap;
 import edu.stanford.nlp.util.CoreMap;
-import edu.stanford.nlp.util.Generics;
 import eu.fbk.dkm.pikes.tintop.annotators.DepParseInfo;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -31,96 +29,96 @@ import java.util.*;
 
 public class StanfordTest {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(StanfordTest.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(StanfordTest.class);
 
-	public static void printTree(Tree tree, int depth) {
-		StringBuffer stringBuffer = new StringBuffer();
-		stringBuffer.append(StringUtils.repeat("-", depth));
-		stringBuffer.append(" ");
-		stringBuffer.append(tree.label());
-		stringBuffer.append(" ");
-		stringBuffer.append(tree.getLeaves());
+    public static void printTree(Tree tree, int depth) {
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append(StringUtils.repeat("-", depth));
+        stringBuffer.append(" ");
+        stringBuffer.append(tree.label());
+        stringBuffer.append(" ");
+        stringBuffer.append(tree.getLeaves());
 
-		if (tree.isLeaf()) {
-			stringBuffer.append(" [leaf]");
-		}
+        if (tree.isLeaf()) {
+            stringBuffer.append(" [leaf]");
+        }
 
-		if (((CoreLabel) tree.label()).containsKey(RNNCoreAnnotations.PredictedClass.class)) {
-			stringBuffer.append(" ");
-			stringBuffer.append("Predicted Class: " + RNNCoreAnnotations.getPredictedClass(tree));
-		}
-		stringBuffer.append("|");
-		stringBuffer.append(RNNCoreAnnotations.getNodeVector(tree));
-		stringBuffer.append("|");
-		stringBuffer.append(RNNCoreAnnotations.getPredictions(tree));
+        if (((CoreLabel) tree.label()).containsKey(RNNCoreAnnotations.PredictedClass.class)) {
+            stringBuffer.append(" ");
+            stringBuffer.append("Predicted Class: " + RNNCoreAnnotations.getPredictedClass(tree));
+        }
+        stringBuffer.append("|");
+        stringBuffer.append(RNNCoreAnnotations.getNodeVector(tree));
+        stringBuffer.append("|");
+        stringBuffer.append(RNNCoreAnnotations.getPredictions(tree));
 
-		System.out.println(stringBuffer.toString());
-		for (Tree t : tree.children()) {
-			printTree(t, depth + 1);
-		}
-	}
+        System.out.println(stringBuffer.toString());
+        for (Tree t : tree.children()) {
+            printTree(t, depth + 1);
+        }
+    }
 
-	public static void removeAts(Tree node) {
-		node.label().setValue(node.label().value().replace("@", ""));
-		for (Tree child : node.children()) {
-			removeAts(child);
-		}
-	}
+    public static void removeAts(Tree node) {
+        node.label().setValue(node.label().value().replace("@", ""));
+        for (Tree child : node.children()) {
+            removeAts(child);
+        }
+    }
 
-	public static void addHeads(Tree node) {
-		addHeads(node, null, null);
-	}
+    public static void addHeads(Tree node) {
+        addHeads(node, null, null);
+    }
 
-	public static void addHeads(Tree node, Tree parent, HeadFinder headFinder) {
-		if (node == null || node.isLeaf()) {
-			return;
-		}
+    public static void addHeads(Tree node, Tree parent, HeadFinder headFinder) {
+        if (node == null || node.isLeaf()) {
+            return;
+        }
 
-		if (headFinder == null) {
-			headFinder = new CollinsHeadFinder();
-		}
+        if (headFinder == null) {
+            headFinder = new CollinsHeadFinder();
+        }
 
-		Tree head = headFinder.determineHead(node, parent);
-		if (!head.isLeaf()) {
-			head.label().setValue(head.label().toString() + "=H");
-		}
+        Tree head = headFinder.determineHead(node, parent);
+        if (!head.isLeaf()) {
+            head.label().setValue(head.label().toString() + "=H");
+        }
 
-		for (Tree child : node.children()) {
-			addHeads(child, node, headFinder);
-		}
+        for (Tree child : node.children()) {
+            addHeads(child, node, headFinder);
+        }
 
-	}
+    }
 
-	private static String space(int width) {
-		StringBuilder b = new StringBuilder();
-		for (int i = 0; i < width; i++) {
-			b.append(' ');
-		}
-		return b.toString();
-	}
+    private static String space(int width) {
+        StringBuilder b = new StringBuilder();
+        for (int i = 0; i < width; i++) {
+            b.append(' ');
+        }
+        return b.toString();
+    }
 
-	// helper for toString()
-	private static void recToString(IndexedWord curr, StringBuilder sb, int offset, Set<IndexedWord> used,
-			SemanticGraph dependencies, HashMap<Integer, String> depLabels, HashMap<Integer, Integer> depParents) {
-		used.add(curr);
-		List<SemanticGraphEdge> edges = dependencies.outgoingEdgeList(curr);
-		Collections.sort(edges);
-		for (SemanticGraphEdge edge : edges) {
-			IndexedWord target = edge.getTarget();
-			depParents.put(target.index(), edge.getSource().index());
-			depLabels.put(target.index(), edge.getRelation().toString());
-			sb.append(space(2 * offset)).append("-> ").append(target).append(" (").append(edge.getRelation()).append(")\n");
-			if (!used.contains(target)) { // recurse
-				recToString(target, sb, offset + 1, used, dependencies, depLabels, depParents);
-			}
-		}
-	}
+    // helper for toString()
+    private static void recToString(IndexedWord curr, StringBuilder sb, int offset, Set<IndexedWord> used,
+            SemanticGraph dependencies, HashMap<Integer, String> depLabels, HashMap<Integer, Integer> depParents) {
+        used.add(curr);
+        List<SemanticGraphEdge> edges = dependencies.outgoingEdgeList(curr);
+        Collections.sort(edges);
+        for (SemanticGraphEdge edge : edges) {
+            IndexedWord target = edge.getTarget();
+            depParents.put(target.index(), edge.getSource().index());
+            depLabels.put(target.index(), edge.getRelation().toString());
+            sb.append(space(2 * offset)).append("-> ").append(target).append(" (").append(edge.getRelation())
+                    .append(")\n");
+            if (!used.contains(target)) { // recurse
+                recToString(target, sb, offset + 1, used, dependencies, depLabels, depParents);
+            }
+        }
+    }
 
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
-
-		Properties props = new Properties();
-		props.setProperty("annotators", "tokenize, ssplit, pos, parse");
+        Properties props = new Properties();
+        props.setProperty("annotators", "tokenize, ssplit, pos, parse");
 //		props.setProperty("annotators", "tokenize, ssplit, dbps");
 //		props.setProperty("tokenize.whitespace", "true");
 //		props.setProperty("ssplit.eolonly", "true");
@@ -171,26 +169,25 @@ public class StanfordTest {
 //
 //		// ---
 
-		System.out.println("Loading other annotators");
-		StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+        System.out.println("Loading other annotators");
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
 
+        String onlyText = "G. W. Bush and Bono are very strong supporters of the fight of HIV in Africa. Their March 2002 meeting resulted in a 5 billion dollar aid.";
+        Annotation s = new Annotation(onlyText);
 
-		String onlyText = "G. W. Bush and Bono are very strong supporters of the fight of HIV in Africa. Their March 2002 meeting resulted in a 5 billion dollar aid.";
-		Annotation s = new Annotation(onlyText);
+        Annotation myDoc = new Annotation(s);
+        pipeline.annotate(myDoc);
 
-		Annotation myDoc = new Annotation(s);
-		pipeline.annotate(myDoc);
+        List<CoreMap> sents = myDoc.get(CoreAnnotations.SentencesAnnotation.class);
+        for (CoreMap thisSent : sents) {
 
-		List<CoreMap> sents = myDoc.get(CoreAnnotations.SentencesAnnotation.class);
-		for (CoreMap thisSent : sents) {
+            SemanticGraph dependencies = thisSent.get(SemanticGraphCoreAnnotations.BasicDependenciesAnnotation.class);
+            DepParseInfo info = new DepParseInfo(dependencies);
 
-			SemanticGraph dependencies = thisSent.get(SemanticGraphCoreAnnotations.BasicDependenciesAnnotation.class);
-			DepParseInfo info = new DepParseInfo(dependencies);
+            System.out.println(info.getDepParents().size());
+            System.out.println(info.getDepLabels().size());
 
-			System.out.println(info.getDepParents().size());
-			System.out.println(info.getDepLabels().size());
-
-			System.out.println(dependencies);
+            System.out.println(dependencies);
 
 //			System.out.println(dependencies.getFirstRoot().index());
 //			System.out.println(dependencies.getChildren(dependencies.getFirstRoot()));
@@ -208,7 +205,7 @@ public class StanfordTest {
 //				System.out.println(token.get(PikesAnnotations.DBpediaSpotlightAnnotation.class));
 //				System.out.println();
 //			}
-		}
+        }
 
 //			System.out.println(s);
 
@@ -230,46 +227,45 @@ public class StanfordTest {
 //				}
 //			}
 
-		System.exit(1);
+        System.exit(1);
 
-		// read some text in the text variable
-		String text = "This is the worst movie I've ever watched!";
+        // read some text in the text variable
+        String text = "This is the worst movie I've ever watched!";
 
-		// create an empty Annotation just with the given text
-		Annotation document = new Annotation(text);
+        // create an empty Annotation just with the given text
+        Annotation document = new Annotation(text);
 
-		// run all Annotators on this text
-		pipeline.annotate(document);
+        // run all Annotators on this text
+        pipeline.annotate(document);
 
 //		System.out.println(document.keySet());
 //		System.exit(1);
 
-		PrintWriter out;
-		out = new PrintWriter(System.out);
+        PrintWriter out;
+        out = new PrintWriter(System.out);
 
-
-		List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
-		if (sentences != null && sentences.size() > 0) {
-			ArrayCoreMap sentence = (ArrayCoreMap) sentences.get(0);
+        List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
+        if (sentences != null && sentences.size() > 0) {
+            ArrayCoreMap sentence = (ArrayCoreMap) sentences.get(0);
 
 //			out.println("Sentence's keys: ");
 //			out.println(sentence.keySet());
 
 //			Tree tree2 = sentence.get(SentimentCoreAnnotations.SentimentAnnotatedTree.class);
-			System.out.println("Sentiment class name:");
+            System.out.println("Sentiment class name:");
 //			System.out.println(sentence.get(SentimentCoreAnnotations.ClassName.class));
 //			System.out.println(RNNCoreAnnotations.getPredictedClass(tree2));
 
-			ArrayList<CoreLabel> indexedTokens = new ArrayList<>();
-			HashMap<Word, CoreLabel> indexedWords = new HashMap<>();
+            ArrayList<CoreLabel> indexedTokens = new ArrayList<>();
+            HashMap<Word, CoreLabel> indexedWords = new HashMap<>();
 
-			List<CoreLabel> tokens = sentence.get(CoreAnnotations.TokensAnnotation.class);
-			for (CoreLabel token : tokens) {
-				indexedTokens.add(token);
-			}
-			System.out.println(indexedTokens);
+            List<CoreLabel> tokens = sentence.get(CoreAnnotations.TokensAnnotation.class);
+            for (CoreLabel token : tokens) {
+                indexedTokens.add(token);
+            }
+            System.out.println(indexedTokens);
 
-			int i = -1;
+            int i = -1;
 //			for (Tree t : tree2.getLeaves()) {
 //				i++;
 //
@@ -284,11 +280,10 @@ public class StanfordTest {
 //				Tree tree = treeIterator.next();
 //				System.out.println(((CoreLabel) tree.label()).get(RNNCoreAnnotations.PredictedClass.class));
 //			}
-		}
+        }
 
-
-		// these are all the sentences in this document
-		// a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
+        // these are all the sentences in this document
+        // a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
 //		List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
 //
 //		for (
@@ -302,5 +297,5 @@ public class StanfordTest {
 //
 //		}
 
-	}
+    }
 }
