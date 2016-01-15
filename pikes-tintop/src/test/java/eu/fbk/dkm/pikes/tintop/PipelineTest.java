cce4e7f3d958bcfc9b882fde3e3db98a52eab3af
Alessio Palmero Aprosio <alessio@apnetwork.it>
2016-01-15 19:00:31 +0100

    Pipeline test file

---
+++ pikes-tintop/src/test/java/eu/fbk/dkm/pikes/tintop/PipelineTest.java
@@ -0,0 +1,14 @@
+package eu.fbk.dkm.pikes.tintop;
+
+import eu.fbk.dkm.pikes.tintop.server.PipelineServer;
+
+/**
+ * Created by alessio on 15/01/16.
+ */
+
+public class PipelineTest {
+
+    public static void main(String[] args) {
+        PipelineServer.main(args);
+    }
+}
--- test/config.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export WORDNET_HOME=~/Documents/Resources/wn-3.0-dict/dict/
-export RDFPRO_PATH=~/Downloads/rdfpro/rdfpro
