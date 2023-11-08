90dcc62fa169c434367ac421da91428ca78e9e72
Benedikt Heinrichs <49311227+BenediktHeinrichs@users.noreply.github.com>
2023-11-08 15:41:22 +0000

    Skip gpg

--- build.sh
+++ build.sh
@@ -1,28 +1,17 @@
-apt-get update && apt-get install -y git gnupg
-gpg --batch --passphrase "pikes" --generate-key <<EOF
-Key-Type: RSA
-Key-Length: 2048
-Subkey-Type: RSA
-Subkey-Length: 2048
-Name-Real: pikes
-Name-Email: example@example.com
-Expire-Date: 0
-Passphrase: pikes
-%commit
-EOF
+apt-get update && apt-get install -y git
 
 git clone https://github.com/fbk/utils \
   && cd utils \
-  && mvn clean install \
+  && mvn clean install -Dgpg.skip \
   && cd .. \
   && git clone https://github.com/fbk/fcw \
   && cd fcw \
   && git checkout develop \
-  && mvn clean install \
+  && mvn clean install -Dgpg.skip \
   && cd .. \
   && git clone https://github.com/dhfbk/tint \
   && cd tint \
   && git checkout develop \
-  && mvn clean install \
+  && mvn clean install -Dgpg.skip \
   && cd .. \
-  && mvn clean package -DskipTests -Prelease
+  && mvn clean package -DskipTests -Dgpg.skip -Prelease
