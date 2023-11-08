a9d8d075a77284adabfbc2d387016b19f545ffbe
Benedikt Heinrichs <49311227+BenediktHeinrichs@users.noreply.github.com>
2023-11-08 15:44:01 +0000

    Skip tests

--- build.sh
+++ build.sh
@@ -2,16 +2,16 @@ apt-get update && apt-get install -y git
 
 git clone https://github.com/fbk/utils \
   && cd utils \
-  && mvn clean install -Dgpg.skip \
+  && mvn clean install -DskipTests -Dgpg.skip \
   && cd .. \
   && git clone https://github.com/fbk/fcw \
   && cd fcw \
   && git checkout develop \
-  && mvn clean install -Dgpg.skip \
+  && mvn clean install -DskipTests -Dgpg.skip \
   && cd .. \
   && git clone https://github.com/dhfbk/tint \
   && cd tint \
   && git checkout develop \
-  && mvn clean install -Dgpg.skip \
+  && mvn clean install -DskipTests -Dgpg.skip \
   && cd .. \
   && mvn clean package -DskipTests -Dgpg.skip -Prelease
