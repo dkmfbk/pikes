6b0b5a5f295f67edf858770f966f9d4a47de8da7
Benedikt Heinrichs <49311227+BenediktHeinrichs@users.noreply.github.com>
2023-11-14 09:33:26 +0000

    Update versions

--- getDependencies.sh
+++ getDependencies.sh
@@ -4,6 +4,6 @@ mv dict wordnet
 wget https://git.rwth-aachen.de/coscine/research/pikesdocker/-/archive/main/pikesdocker-main.tar.gz?path=models
 tar -zxvf pikesdocker-main.tar.gz?path=models
 mv pikesdocker-main-models/models models
-wget https://git.rwth-aachen.de/coscine/research/pikesdocker/-/archive/main/pikesdocker-main.tar.gz?path=ukb
-tar -zxvf pikesdocker-main.tar.gz?path=ukb
-mv pikesdocker-main-ukb/ukb ukb
+wget --no-check-certificate https://ixa2.si.ehu.eus/ukb/ukb_3.2.tgz
+tar -zxvf ukb_3.2.tgz
+mv ukb-3.2/bin ukb
